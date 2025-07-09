d6a047cfeeed6a4a0ce4e971162c5b040f82b94e
diegosadock <diegosadock1234@gmail.com>
2025-07-09 16:29:20 -0300

    adds firsts IsiSpring `annotations`

---
+++ src/main/java/br/com/sadock/isispring/annotations/IsiController.java
@@ -0,0 +1,13 @@
+package br.com.sadock.isispring.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface IsiController {
+
+}
