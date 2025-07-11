97b67596b3d12eb5eaa72636dbcf065fea85be98
diegosadock <diegosadock1234@gmail.com>
2025-07-10 22:07:40 -0300

    adds invoking to POST Methods with parameter

--- src/main/java/br/com/sadock/isispring/web/IsiDispatchServlet.java
+++ src/main/java/br/com/sadock/isispring/web/IsiDispatchServlet.java
@@ -1,8 +1,11 @@
 package br.com.sadock.isispring.web;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 
 import com.google.gson.Gson;
 
@@ -43,33 +46,63 @@ public class IsiDispatchServlet extends HttpServlet {
 			controller = ControllersInstances.instances.get(data.controllerClass);
 			if (controller == null) {
 				IsiLogger.log("IsiDispatchServlet", "Creating new Controller Instance...");
-				controller  = Class.forName(data.controllerClass).getDeclaredConstructor().newInstance();
+				controller = Class.forName(data.controllerClass).getDeclaredConstructor().newInstance();
 				ControllersInstances.instances.put(data.controllerClass, controller);
 			}
-			
-			// preciso extrair o método desta classe, ou seja, aquele método que vai atender a requisição
+
+			// preciso extrair o método desta classe, ou seja, aquele método que vai atender
+			// a requisição
 			Method controllerMethod = null;
-			
-			for (Method  method : controller.getClass().getMethods()) {
+
+			for (Method method : controller.getClass().getMethods()) {
 				if (method.getName().equals(data.controllerMethod)) {
 					controllerMethod = method;
 					break;
 				}
 			}
-			
+
 			IsiLogger.log("IsiDispatchServlet", "Invoking Method " + controllerMethod.getName() + " to handle request");
-			out.println(gson.toJson(controllerMethod.invoke(controller)));
+
+			// meu método tem parâmetros?
+			if (controllerMethod.getParameterCount() > 0) {
+				IsiLogger.log("IsiDispatchServlet", "Method " + controllerMethod.getName() + " has parameters");
+				Object arg;
+				Parameter parameter = controllerMethod.getParameters()[0];
+
+				if (parameter.getAnnotations()[0].annotationType().getName()
+						.equals("br.com.sadock.isispring.annotations.IsiBody")) {
+					// preciso ler os dados que vem da requisição
+					String body = readBytesFromRequest(request);
+					IsiLogger.log("", "     Found Parameter from request of type " + parameter.getType().getName());
+					IsiLogger.log("", "     Parameter content " + body);
+					arg = gson.fromJson(body, parameter.getType());
+
+					out.println(gson.toJson(controllerMethod.invoke(controller, arg)));
+				} 
+				else {
+					out.println(gson.toJson(controllerMethod.invoke(controller)));
+				}
+			}
+
 			out.close();
 			// executar o método e (no fundo, escrever na saída a execução dele)
-			
-			
+
 		} 
 		catch (Exception ex) {
 			ex.printStackTrace();
 		}
 
-		
+	}
 
+	private String readBytesFromRequest(HttpServletRequest request) throws Exception {
+		StringBuilder str = new StringBuilder();
+		String line;
+		BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
+
+		while ((line = br.readLine()) != null) {
+			str.append(line);
+		}
+		return str.toString();
 	}
 
 }
